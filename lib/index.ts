05f8c29211d1939edb6c57b15ea75180639c3973
Ana Ulin <ana@ulin.org>
2019-01-15 15:49:43 -0800

    Use cy.request to post domSnapshot directly to agent process. Turn back on chromeWebSecurity to demonstrate that this avoids CSP restrictions.

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import {clientInfo, environmentInfo} from './environment'
 import PercyAgent from '@percy/agent'
 
@@ -14,24 +13,21 @@ Cypress.Commands.add('percySnapshot', (name: string, options: any = {}) => {
 
   name = name || cy.state('runnable').fullTitle()
 
-  const domSnapshot = cy.document().then((doc: Document) => {
+  cy.document().then((doc: Document) => {
     options.document = doc
     const domSnapshot = percyAgentClient.snapshot(name, options)
-
-    return axios.post(`http://localhost:${percyAgentClient.port}/percy/snapshot`, {
-      name,
-      url: doc.URL,
-      enableJavaScript: options.enableJavaScript,
-      widths: options.widths,
-      clientInfo,
-      environmentInfo,
-      domSnapshot,
-    })
-    .then((response: any) => {
-      cy.log(`[percy] Snapshot ${name} successfully created.`)
-    })
-    .catch((error: any) => {
-      cy.log('[percy] Something went wrong creating a snapshot.')
+    return cy.request({
+      method: 'POST',
+      url: `http://localhost:${percyAgentClient.port}/percy/snapshot`,
+      body: {
+        name,
+        url: doc.URL,
+        enableJavaScript: options.enableJavaScript,
+        widths: options.widths,
+        clientInfo,
+        environmentInfo,
+        domSnapshot
+      }
     })
   })
 })
