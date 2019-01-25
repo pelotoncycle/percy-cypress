9f4a5bf33634b47f1474e8dd824c73c5449defee
David Jones <dgjones@gmail.com>
2019-01-25 14:22:11 -0800

    export typings for custom percySnapshot command

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,26 @@
 import {clientInfo, environmentInfo} from './environment'
 import PercyAgent from '@percy/agent'
+import {SnapshotOptions} from '@percy/agent/dist/percy-agent-client/snapshot-options'
 
 declare const Cypress: any
 declare const cy: any
 
-Cypress.Commands.add('percySnapshot', (name: string, options: any = {}) => {
+export declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Take a snapshot in Percy
+     * @see https://github.com/percy/percy-cypress
+     * @example
+     * ```
+     * cy.percySnapshot(name: 'home page')
+     * cy.percySnapshot(name: 'home page', {widths: [1280, 1960]})
+     * ```
+     */
+    percySnapshot(name: string, options: SnapshotOptions): Chainable
+  }
+}
+
+Cypress.Commands.add('percySnapshot', (name: string, options: SnapshotOptions = {}) => {
   const percyAgentClient = new PercyAgent({
     clientInfo: clientInfo(),
     environmentInfo: environmentInfo()
